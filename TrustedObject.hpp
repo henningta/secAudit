af1832d19e5c075fca08d1724691b702d1da4b07
Jackson G. Reed <reed40@sslab10.cs.purdue.edu>
2014-10-26 22:03:43 -0400

    Fixed last commit

--- TrustedObject.hpp
+++ TrustedObject.hpp
@@ -7,7 +7,7 @@
  * Represents the trusted machine which is used to generate the initial key
  * as well as perform other encryption-related functions
  *
- * @author(s)	Travis Henning
+ * @author(s)	Travis Henning Jackson Reed
  */
 
 #include <string>
@@ -24,18 +24,14 @@ private:
   std::map< std::string , std::string > keyTable;
 
 public:
-<<<<<<< HEAD
   TrustedObject(EVP_PKEY PublicKey,EVP_PKEY PrivateKey);
   //This is temporary so the code compiles, Dont use this
   TrustedObject();
   //ID needs to be unique!
+  //TODO:
   void addPublicKeys(std::string ID, std::string PublicKey);
 
   Message verifyInitMessage(Message M0);
-=======
-	void verifyInitMessage();	// TODO
-	void generateStuff();		// TODO
->>>>>>> 1979d48bb4468ca5668c6d769cabe3836bf4cdce
 
 private:
   MessageMaker mkr;
