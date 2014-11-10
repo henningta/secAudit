6b279f6f42004e2d1f2229fae923cc0a224aeb29
Jackson G. Reed <reed40@sslab10.cs.purdue.edu>
2014-11-09 20:30:40 -0500

    Added verification code prototype.

--- TrustedObject.hpp
+++ TrustedObject.hpp
@@ -18,17 +18,15 @@ class TrustedObject {
 private:
   std::string _keyA0;
   Message M0;
-  EVP_PKEY mPublic;
-  EVP_PKEY mPrivate;
-  std::map< std::string , std::string > keyTable;
+  EVP_PKEY * mPublic;
+  EVP_PKEY * mPrivate;
+  std::map< std::string , EVP_PKEY * > keyTable;
 
 public:
-  TrustedObject(EVP_PKEY PublicKey,EVP_PKEY PrivateKey);
-  //This is temporary so the code compiles, Dont use this
   TrustedObject();
   //ID needs to be unique!
   //TODO:
-  void addPublicKeys(std::string ID, std::string PublicKey);
+  void addPublicKeys(std::string ID, EVP_PKEY * PublicKey);
 
   Message verifyInitMessage(Message M0);
 
